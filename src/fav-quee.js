8c9fea7555c370d811b1a63cef3ed4b9aef4020a
Troy Daniels <troy_daniels@live.com.au>
2016-05-29 18:57:55 +1000

    Working implementation

---
+++ src/fav-quee.js
@@ -0,0 +1,43 @@
+/* fav-quee.js - Add marquee text to your favicon.
+ * 
+ * Troy Daniels 29/05/16
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var interval = 150,  // Step time in milliseconds
+    favText = document.getElementById('favicon').getAttribute('data-text'),
+    textColour = document.getElementById('favicon').getAttribute('data-col'),
+    favSize = offset = 16,  // Size of favicon, and L-R offset position of text
+    canvas = document.createElement('canvas'),
+    img = document.createElement('img'),
+    link = document.getElementById('favicon').cloneNode(true);
+
+canvas.height = canvas.width = favSize;
+img.src = document.getElementById('favicon').getAttribute('href');
+
+// And, go...
+window.setInterval(updateFavicon, interval);
+
+function updateFavicon() {
+   if (canvas.getContext) {
+      ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+      ctx.font = 'bold 10px "helvetica", sans-serif';
+      ctx.fillStyle = textColour;
+      ctx.fillText(favText, offset==-(favText.length*5)?(offset=favSize):offset--, 12);
+      link.href = canvas.toDataURL();
+      document.body.appendChild(link);
+   }
+}
